17da8509b92c713fa3dec4e5f7a02dcd252c0536
mishatunikov <michael.tunikov@gmail.com>
2025-05-21 14:03:54 +0300

    feat: Create base project models

--- django_backend/shop/models.py
+++ django_backend/shop/models.py
@@ -1,3 +1,156 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from shop import consts
 
-# Create your models here.
+
+class CreatedUpdatedAt(models.Model):
+    """Abstract model with creation and update datetime."""
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-created_at',)
+
+
+class BaseName(models.Model):
+    """Abstract model with name field."""
+
+    name = models.CharField(
+        max_length=consts.MAX_NAME_LENGTH, verbose_name='название'
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class TgUser(CreatedUpdatedAt):
+    """Telegram User model."""
+
+    tg_id = models.PositiveBigIntegerField(
+        primary_key=True,
+        verbose_name='tg_id',
+    )
+    username = models.CharField(
+        max_length=consts.MAX_NAME_LENGTH,
+        verbose_name='псевдоним',
+        null=True,
+        unique=True,
+        blank=True,
+    )
+    first_name = models.CharField(
+        max_length=consts.MAX_NAME_LENGTH,
+        blank=True,
+        verbose_name='имя',
+    )
+    last_name = models.CharField(
+        max_length=consts.MAX_NAME_LENGTH, blank=True, verbose_name='фамилия'
+    )
+    is_active = models.BooleanField(default=True, verbose_name='активен')
+
+    class Meta(CreatedUpdatedAt.Meta):
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'User(tg_id={self.tg_id})'
+
+
+class Category(BaseName, CreatedUpdatedAt):
+    """Category model."""
+
+    slug = models.SlugField(
+        max_length=consts.MAX_SLUG_LENGTH,
+        verbose_name='идентификатор',
+        unique=True,
+    )
+
+    class Meta(BaseName.Meta):
+        verbose_name = 'категория'
+        verbose_name_plural = 'Категории'
+
+
+class SubCategory(Category):
+    """Subcategory model."""
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='категория',
+        related_name='subcategories',
+    )
+
+    class Meta(Category.Meta):
+        verbose_name = 'подкатегория'
+        verbose_name_plural = 'Подкатегории'
+
+
+class Item(BaseName):
+    """Item model."""
+
+    description = models.TextField(
+        max_length=consts.MAX_TEXT_LENGTH, verbose_name='описание'
+    )
+    image = models.ImageField(upload_to='items', verbose_name='изображение')
+    price = models.DecimalField(
+        max_digits=consts.DECIMAL_MAX_DIGITS,
+        decimal_places=consts.DECIMAL_PLACES,
+        verbose_name='цена (р.)',
+        validators=[
+            MinValueValidator(consts.MIN_ITEM_PRICE),
+            MaxValueValidator(consts.MAX_ITEM_PRICE),
+        ],
+    )
+    subcategory = models.ForeignKey(
+        SubCategory,
+        on_delete=models.CASCADE,
+        related_name='items',
+        verbose_name='подкатегория',
+    )
+
+    class Meta(BaseName.Meta):
+        verbose_name = 'товар'
+        verbose_name_plural = 'Товары'
+
+
+class ShoppingCart(CreatedUpdatedAt):
+    user = models.OneToOneField(
+        TgUser,
+        on_delete=models.CASCADE,
+        related_name='shopping_cart',
+        verbose_name='пользователь',
+    )
+
+    class Meta:
+        ordering = ('user', 'created_at')
+        verbose_name = 'корзина'
+        verbose_name_plural = 'Корзины'
+
+
+class ShoppingCartItems(CreatedUpdatedAt):
+    shopping_cart = models.ForeignKey(
+        ShoppingCart, on_delete=models.CASCADE, related_name='items'
+    )
+    item = models.ForeignKey(
+        Item, on_delete=models.CASCADE, related_name='shopping_cart'
+    )
+    amount = models.PositiveSmallIntegerField(
+        validators=[
+            MinValueValidator(consts.MIN_ITEM_AMOUNT),
+            MaxValueValidator(consts.MAX_ITEM_AMOUNT),
+        ]
+    )
+
+    class Meta(CreatedUpdatedAt.Meta):
+        verbose_name = 'товар корзины'
+        verbose_name_plural = 'Товары корзины'
+        constraints = [
+            models.UniqueConstraint(
+                fields=('shopping_cart', 'item'), name='item_shopping_cart'
+            )
+        ]
