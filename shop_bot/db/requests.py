7308cf3c4c861c8cfad1b4f7b75448688d57ce8b
mishatunikov <michael.tunikov@gmail.com>
2025-05-22 23:56:06 +0300

    chore: create a module for requests in database through django orm

---
+++ shop_bot/db/requests.py
@@ -0,0 +1,35 @@
+import os
+
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
+os.environ.update({'DJANGO_ALLOW_ASYNC_UNSAFE': 'true'})
+django.setup()
+
+import logging
+from asgiref.sync import sync_to_async
+
+from shop.models import TgUser, Category
+
+
+logger = logging.getLogger(__name__)
+
+
+@sync_to_async
+def add_or_create_user(user_id: int, **kwargs) -> TgUser:
+    user, created = TgUser.objects.get_or_create(
+        tg_id=user_id, defaults=kwargs
+    )
+
+    if created:
+        logger.info(f'Пользователь tg_id={user_id} был добавлен в бд.')
+
+    else:
+        logger.info(f'Пользователь tg_id={user_id} уже существует в бд.')
+
+    return user
+
+
+@sync_to_async
+def get_categories():
+    return Category.objects.all()
