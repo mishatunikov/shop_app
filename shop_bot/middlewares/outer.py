6db4e91faca80ab4816561b7646dbfef3c9f212b
mishatunikov <michael.tunikov@gmail.com>
2025-05-22 14:25:13 +0300

    feat: create TranslatorRunnerMiddleware for injection i18n

---
+++ shop_bot/middlewares/outer.py
@@ -0,0 +1,20 @@
+from typing import Any, Awaitable, Callable, Dict
+
+import consts
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from fluentogram import TranslatorHub
+
+
+class TranslatorRunnerMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any],
+    ) -> Any:
+        hub: TranslatorHub = data.get('_translator_hub')
+        data['i18n'] = hub.get_translator_by_locale(
+            locale=consts.DEFAULT_LOCALE
+        )
+        return await handler(event, data)
