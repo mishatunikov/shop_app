1b313fc0bc94605b95b535616fe8e7c6728bd4a7
mishatunikov <michael.tunikov@gmail.com>
2025-05-23 00:00:42 +0300

    feat: create functional of start handler

--- shop_bot/handlers/user_handlers.py
+++ shop_bot/handlers/user_handlers.py
@@ -1,13 +1,17 @@
 import logging
 
+from aiogram import Router
 from aiogram.filters import (
-    CommandStart,
-    ChatMemberUpdatedFilter,
     KICKED,
     MEMBER,
+    ChatMemberUpdatedFilter,
+    CommandStart,
 )
-from aiogram.types import Message, ChatMemberUpdated
-from aiogram import Router
+from aiogram.types import ChatMemberUpdated, Message
+from aiogram_dialog import DialogManager, StartMode
+
+from db.requests import add_or_create_user
+from dialogs.start_dialog.states import StartSG
 
 router = Router()
 
@@ -16,13 +20,23 @@ logger = logging.getLogger(__name__)
 
 
 @router.message(CommandStart)
-def start(message: Message):
+async def start(message: Message, dialog_manager: DialogManager):
     logger.info(f'Пользователь id={message.from_user.id} активировал бота.')
+    await add_or_create_user(
+        message.from_user.id,
+        first_name=message.from_user.first_name,
+        last_name=message.from_user.last_name,
+        username=message.from_user.username,
+    )
+    await dialog_manager.start(
+        state=StartSG.main_menu,
+        mode=StartMode.RESET_STACK,
+        data={'first_open': True},
+    )
 
 
 @router.my_chat_member(ChatMemberUpdatedFilter(member_status_changed=KICKED))
 async def block_bot(event: ChatMemberUpdated):
-    logger.info(f'{event.new_chat_member.status}')
     logger.info(f'Пользователь id={event.from_user.id} заблокировал бота.')
 
 
