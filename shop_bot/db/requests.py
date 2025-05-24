74c82a6c48ff65877133b4d80e21aeaf4c8df60a
mishatunikov <michael.tunikov@gmail.com>
2025-05-24 19:59:49 +0300

    feat: create new functions for db request

--- shop_bot/db/requests.py
+++ shop_bot/db/requests.py
@@ -1,6 +1,7 @@
 import os
-import django
+from decimal import Decimal
 
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_backend.settings')
 os.environ.update({'DJANGO_ALLOW_ASYNC_UNSAFE': 'true'})
@@ -9,8 +10,17 @@ django.setup()
 import logging
 from asgiref.sync import sync_to_async
 
-from django.db.models import Sum, Count, OuterRef, Exists, Subquery
-from django.db.models import IntegerField
+from django.db.models import (
+    Sum,
+    Count,
+    OuterRef,
+    Exists,
+    Subquery,
+    ExpressionWrapper,
+    F,
+    QuerySet,
+)
+from django.db.models import IntegerField, DecimalField
 
 from shop.models import TgUser, Category, Subcategory, Item, ShoppingCartItems
 
@@ -35,7 +45,7 @@ def add_or_create_user(user_id: int, **kwargs) -> TgUser:
 
 
 @sync_to_async
-def get_active_categories():
+def get_active_categories() -> QuerySet:
     """Return active category, which have at least one item."""
 
     return (
@@ -48,7 +58,7 @@ def get_active_categories():
 
 
 @sync_to_async
-def get_active_subcategories(category_name: str):
+def get_active_subcategories(category_name: str) -> QuerySet:
     """Return active subcategory, which have at least on item."""
 
     return (
@@ -60,7 +70,7 @@ def get_active_subcategories(category_name: str):
 
 
 @sync_to_async
-def get_items(subcategory_name: str, tg_id: int):
+def get_items(subcategory_name: str, tg_id: int) -> QuerySet:
     """Return subcategory items."""
 
     cart_items = ShoppingCartItems.objects.filter(
@@ -76,7 +86,7 @@ def get_items(subcategory_name: str, tg_id: int):
 
 
 @sync_to_async
-def increase_shopping_cart(tg_id, item_id, amount):
+def increase_shopping_cart(tg_id, item_id, amount) -> ShoppingCartItems:
     """Add item to user shopping cart."""
 
     instance = ShoppingCartItems(user_id=tg_id, item_id=item_id, amount=amount)
@@ -85,9 +95,69 @@ def increase_shopping_cart(tg_id, item_id, amount):
 
 
 @sync_to_async
-def decrease_shopping_cart(user_id, item_id):
+def decrease_shopping_cart(user_id, item_id) -> tuple[int, dict]:
     """Remove item from user shopping cart."""
 
     return ShoppingCartItems.objects.get(
         user_id=user_id, item_id=item_id
     ).delete()
+
+
+@sync_to_async
+def get_shopping_cart_items(tg_id: int) -> tuple[QuerySet, bool, int, Decimal]:
+    """Return user shopping cart information."""
+
+    subquery = ShoppingCartItems.objects.filter(
+        item=OuterRef('pk'),
+        user_id=tg_id,
+    ).values('amount')
+
+    items = (
+        Item.objects.filter(shopping_cart__user=tg_id)
+        .annotate(
+            amount=Subquery(
+                subquery,
+                output_field=IntegerField(),
+            )
+        )
+        .annotate(
+            total_item_price=ExpressionWrapper(
+                F('price') * F('amount'), output_field=DecimalField()
+            )
+        )
+    )
+
+    total_amount = items.aggregate(total=Sum('amount'))['total'] or 0
+    total_price = items.aggregate(total=Sum('total_item_price'))['total'] or 0
+
+    return items, items.exists(), total_amount, total_price
+
+
+@sync_to_async
+def clean_shopping_cart(tg_id: int) -> None:
+    """Clean shopping cart."""
+
+    ShoppingCartItems.objects.filter(user_id=tg_id).delete()
+
+
+@sync_to_async()
+def delete_item_from_cart(tg_id: int, item_id: int) -> None:
+    """Delete item from shopping cart."""
+
+    ShoppingCartItems.objects.filter(user_id=tg_id, item_id=item_id).delete()
+
+
+@sync_to_async()
+def get_user_cart_info(tg_id: int) -> bool:
+    """Check shopping cart for void."""
+
+    return TgUser.objects.get(tg_id=tg_id).shopping_cart_items.exists()
+
+
+@sync_to_async
+def change_item_amount(tg_id: int, item_id: int, amount) -> None:
+    """Change item amount in shopping cart."""
+
+    instance = ShoppingCartItems.objects.get(user_id=tg_id, item_id=item_id)
+    instance.amount = amount
+    instance.save()
