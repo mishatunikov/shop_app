0ad2334ec2aff016f011102936fbbc8cfc88c2d4
mishatunikov <michael.tunikov@gmail.com>
2025-05-26 10:13:59 +0300

    feat: add postgresql settings

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -65,12 +65,24 @@ WSGI_APPLICATION = 'django_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if config.django_settings.db_prod:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config.db.db_name,
+            'USER': config.db.db_user,
+            'PASSWORD': config.db.db_password,
+            'HOST': config.db.db_host,
+            'PORT': config.db.db_port,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
